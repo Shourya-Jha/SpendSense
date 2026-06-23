974ffaf5375913e52394a9f3b45691ea2a8a6d7f
SRV-KILLER09 <992501030399@mail.jiit.ac.in>
2026-06-23 13:43:30 +0530

    Integrate expense management features into application

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "include/Expense.h"
+#include "include/Search.h"
 
 using namespace std;
 
 int main()
 {
+    Expense expense;
+    Search search;
+
     int choice;
 
     do
@@ -29,8 +34,67 @@ int main()
                 break;
 
             case 2:
-                cout << "\nExpense Module Coming Soon...\n";
+            {
+                int expenseChoice;
+
+                cout << "\nExpense Management";
+                cout << "\n1. Add Expense";
+                cout << "\n2. View Expense";
+                cout << "\n3. Edit Expense";
+                cout << "\n4. Delete Expense";
+                cout << "\n5. Search by ID";
+                cout << "\n6. Search by Category";
+                cout << "\n7. Search by Date";
+                cout << "\n8. Search by Amount";
+                cout << "\n9. Show Statistics";
+
+                cout << "\nEnter Choice: ";
+                cin >> expenseChoice;
+
+                switch(expenseChoice)
+                {
+                    case 1:
+                        expense.addExpense();
+                        break;
+
+                    case 2:
+                        expense.displayExpense();
+                        break;
+
+                    case 3:
+                        expense.editExpense();
+                        break;
+
+                    case 4:
+                        expense.deleteExpense();
+                        break;
+
+                    case 5:
+                        search.searchByID(expense);
+                        break;
+
+                    case 6:
+                        search.searchByCategory(expense);
+                        break;
+
+                    case 7:
+                        search.searchByDate(expense);
+                        break;
+
+                    case 8:
+                        search.searchByAmount(expense);
+                        break;
+
+                    case 9:
+                        expense.showStatistics();
+                        break;
+
+                    default:
+                        cout << "\nInvalid Choice!\n";
+                }
+
                 break;
+            }
 
             case 3:
                 cout << "\nCategory Module Coming Soon...\n";
