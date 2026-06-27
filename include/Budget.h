0986ed93e09cb27215b6fa360c59a6188ca72d7d
Vardaan Saxena <100420014+SRV-KILLER09@users.noreply.github.com>
2026-06-27 11:22:50 +0530

    Update Budget.h

--- include/Budget.h
+++ include/Budget.h
@@ -3,34 +3,13 @@
 
 class Budget
 {
-private:
+public:
     double monthlyBudget;
     double totalSpent;
 
-public:
-    // Constructor
-    Budget();
-
-    // Set or increase budget
-    void setBudget(double amount);
-    void addBudget(double amount);
-
-    // Record an expense
-    void addExpense(double amount);
-
-    // Getters
-    double getBudget();
-    double getTotalSpent();
-    double getRemainingBudget();
-    double getBudgetUsage();
-
-    // Check budget status
-    bool isBudgetExceeded();
-
-    // Display budget report
+    void setBudget();
+    void addExpense();
     void displayBudget();
-
-    // Reset budget and expenses
     void resetBudget();
 };
 
