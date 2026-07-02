bc0e4d7e759afd552efc2b1194bc6359549ff1ba
devpuri9 <992501030424@mail.jiit.ac.in>
2026-07-03 01:54:28 +0530

    Update TagManager.h

---
+++ include/TagManager.h
@@ -0,0 +1,16 @@
+#ifndef TAGMANAGER_H
+#define TAGMANAGER_H
+
+#include <string>
+
+class TagManager
+{
+public:
+    std::string tagName;
+
+    void addTag();
+    void displayTag();
+    void assignTag();
+};
+
+#endif
